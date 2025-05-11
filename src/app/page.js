13446b3e5356ec7c4656df1ba265836abd052114
notilmi <dznilmi@gmail.com>
2025-05-11 19:50:02 +0700

    Reorder Tailwind classes in Home page

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center">
-      <div className="w-[350px] p-4 rounded-lg flex flex-col gap-4 border shadow-md">
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <div className="flex w-[350px] flex-col gap-4 rounded-lg border p-4 shadow-md">
         <img
           src="https://avatars.githubusercontent.com/u/113961961?v=4"
-          className="size-16 rounded-full hover:animate-spin hover:scale-105 transition-all ease-in-out"
+          className="size-16 rounded-full transition-all ease-in-out hover:scale-105 hover:animate-spin"
         />
         <div>
-          <h1 className="font-bold text-lg">Achmad Ilmi Al Akbar</h1>
+          <h1 className="text-lg font-bold">Achmad Ilmi Al Akbar</h1>
           <p className="text-sm text-gray-500">Pembisnis Digital Handal</p>
         </div>
         <p className="text-gray-600">
@@ -18,8 +18,8 @@ export default function Home() {
           Penggiat Bisnis Digital Yang Katanya Handal"
         </p>
         <Link href="https://www.instagram.com/iilmii._/" className="w-full">
-          <button className="w-full bg-black text-white py-1.5 rounded-md flex items-center justify-center hover:cursor-pointer text-sm hover:scale-105 transition-all ease-in-out">
-            <Instagram className="size-4 mr-2" />
+          <button className="flex w-full items-center justify-center rounded-md bg-black py-1.5 text-sm text-white transition-all ease-in-out hover:scale-105 hover:cursor-pointer">
+            <Instagram className="mr-2 size-4" />
             Instagram Guwe
           </button>
         </Link>
